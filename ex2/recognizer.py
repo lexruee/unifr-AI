788252bda938798d126f4d80202c23725b50cc9f
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2015-10-16 04:35:54 +0200

    update recognizer.py

--- ex2/recognizer.py
+++ ex2/recognizer.py
@@ -8,6 +8,7 @@ import wave
 import numpy.fft as fft
 import struct
 import operator
+import math
 
 train_path = './sounds/train'
 test_path = './sounds/test'
@@ -37,7 +38,7 @@ def read_data():
     return train_data, test_data
 
 
-def recognize(test_signal, train_data, user_key):
+def recognize(test_signal, train_data, user_key, plot):
     stats = {}
 
     for key, train_signal in zip(range(0, 10), train_data):
@@ -45,10 +46,10 @@ def recognize(test_signal, train_data, user_key):
         loss = 0
         # compute the loss function
         for i in range(0, min_size):
-            distance = abs(train_signal[i] - test_signal[i])
-            loss += distance * distance
+            distance = train_signal[i] - test_signal[i]
+            loss += distance**2
 
-        stats[key] = loss
+        stats[key] = loss/2.0
     total_loss = 0
     for key, loss in stats.items():
         total_loss += loss
@@ -57,23 +58,30 @@ def recognize(test_signal, train_data, user_key):
 
     min_key, min_loss = min(stats.iteritems(), key=operator.itemgetter(1))
 
+    print('-------------------------------------------------------')
     print('Predict: %s' % min_key)
     print('User input: %s' % user_key)
     print('Min. loss: %s' % min_loss)
+    print("success: %s" % (min_key == user_key))
     print('')
     for key, prop in stats.items():
         print("label: %s, loss: %s " % (key, round(prop, 2)))
 
-    plt.figure()
-    plt.bar(np.arange(10), stats.values(), align='center', alpha=0.5)
-    plt.xticks(np.arange(10), stats.keys())
-    plt.xlabel('Digits')
-    plt.ylabel('Loss')
+    print('-------------------------------------------------------')
 
-    for key, prop in stats.items():
-        plt.text(key, prop, str(round(prop, 2)))
+    if plot:
+        plt.figure()
+        plt.bar(np.arange(10), stats.values(), align='center', alpha=0.5)
+        plt.xticks(np.arange(10), stats.keys())
+        plt.xlabel('Digits')
+        plt.ylabel('Loss')
 
-    plt.show()
+        for key, prop in stats.items():
+            plt.text(key, prop, str(round(prop, 2)))
+
+        plt.show()
+
+    return min_key
 
 
 def transform_data(data):
@@ -94,15 +102,28 @@ def main():
         # plt.show()
 
 
+    print("Minimalistic digit recognizer - Who needs a GUI?")
+
     loop, user_input = True, None
     while loop:
-        user_input = input("Enter a value from 0-9:")
+        user_input = input("Enter a value from 0-9: ")
         user_input = int(user_input)
         loop = not (0 <= user_input <= 9)
 
     test_signal = test_data[user_input]
-    recognize(test_signal, train_data, user_input)
+    recognize(test_signal, train_data, user_input, True)
+
+
+def evaluate():
+    train_data, test_data = read_data()
+    train_data = transform_data(train_data)
+    test_data = transform_data(test_data)
+
+    for digit in range(0,10):
+        test_signal = test_data[digit]
+        recognize(test_signal, train_data, digit, False)
 
 
 if __name__ == '__main__':
     main()
+    #evaluate()
